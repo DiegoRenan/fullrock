2418f982f2e8ceeb67d4a41a5d12ac1c279a681d
DiegoRenan <renan.ccomp@gmail.com>
2019-02-18 12:39:10 -0300

    Create root Members index

---
+++ app/javascript/components/Members.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Members extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+        <h1>Index</h1>
+    );
+  }
+}
---
+++ app/javascript/packs/members-bundle.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails';
+
+import Members from '../components/Members';
+
+ReactOnRails.register({
+  Members,
+});
